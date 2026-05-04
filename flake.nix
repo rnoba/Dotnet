1644addabe4d3fb7335cde9fa29399e7f447da01
rnoba (Rafael) <130116482+rnoba@users.noreply.github.com>
2026-05-04 18:11:42 -0300

    up

--- flake.nix
+++ flake.nix
@@ -1,12 +1,15 @@
 {
   description = "C# .NET development environment";
+
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
   };
+
   outputs = { self, nixpkgs }:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
+
       dotnet = pkgs.dotnetCorePackages.combinePackages [
         pkgs.dotnetCorePackages.sdk_10_0
       ];
@@ -15,25 +18,28 @@
       lib = {
         mkDotnetShell = { name, extraPackages ? [] }: pkgs.mkShell {
           inherit name;
+
           packages = with pkgs; [
             dotnet
             omnisharp-roslyn
           ] ++ extraPackages;
+
           shellHook = ''
-            echo "${name} ready"
-            echo " SDK Version : $(dotnet --version)"
+            echo "→ ${name} ready"
+            echo "   SDK Version : $(dotnet --version)"
+
             export DOTNET_ROOT="${dotnet}"
-            export DOTNET_CLI_TELEMETRY_OPTOUT=1
+            export DOTNET_CLI_HOME="$PWD/.dotnet"
+            export NUGET_PACKAGES="$PWD/.nuget/packages"
             export DOTNET_SKIP_FIRST_TIME_EXPERIENCE=1
+            export DOTNET_CLI_TELEMETRY_OPTOUT=1
             export DOTNET_NOLOGO=1
 
-            export DOTNET_CLI_HOME="$PWD/.dotnet"
-            export NUGET_PACKAGES="$PWD/.dotnet/packages"
-
-            mkdir -p "$PWD/.dotnet/packages"
             mkdir -p "$PWD/.dotnet/tools"
-
-            export PATH="$PATH:$PWD/.dotnet/tools"
+            mkdir -p "$PWD/.nuget/packages"
+            export PATH="$PWD/.dotnet/tools:$PATH"
+            echo "   Local dotnet home : $PWD/.dotnet"
+            echo "   Local NuGet cache : $PWD/.nuget/packages"
           '';
         };
       };
